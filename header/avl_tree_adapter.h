a88bcea2ccaed582a1560b5eef7ad79de1766794
epeket <165750015+epeket@users.noreply.github.com>
2024-11-17 21:28:36 +0900

    Feat(avl_tree_adapter.h) : adaptee클래스를 AVLTree인터페이스에 맞게 적용하는 클래스 인터페이스

---
+++ header/avl_tree_adapter.h
@@ -0,0 +1,34 @@
+#ifndef OSAP_HEADER_AVLTREE_ADAPTER_H_
+#define OSAP_HEADER_AVLTREE_ADAPTER_H_
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <queue>
+
+#include "avl_tree.h"
+#include "avl_tree_adaptee.h"
+using namespace std;
+
+class AVLTreeAdapter : public AVLTree {
+ public:
+  // 생성자
+  AVLTreeAdapter(AVLTreeAdaptee adaptee);
+
+  // 멤버 함수들
+  void Find(int x) override;
+  void Insert(int x) override;
+  void Empty() override;
+  void Size() override;
+  void Height() override;
+  void Ancestor(int x) override;
+  void Average(int x) override;
+  void Rank(int x) override;
+  void Erase(int x) override;
+
+ private:
+  AVLTreeAdaptee adaptee_;  // 내부적으로 사용할 AVLTreeAdaptee 객체
+};
+
+#endif  // OSAP_HEADER_AVLTREE_ADAPTER_H_
