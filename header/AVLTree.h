0c12ad207a007d8f582ada87bdf521a9d579551f
m020202 <m020202@naver.com>
2024-11-17 13:31:50 +0900

    Feat(AVLTree.h): AVLTree 인터페이스 구현

---
+++ header/AVLTree.h
@@ -0,0 +1,18 @@
+#ifndef OSAP_HEADER_AVLTREE_H_
+#define OSAP_HEADER_AVLTREE_H_
+
+class AVLTree {
+ public:
+  virtual void Find(int x) = 0;
+  virtual void Insert(int x) = 0;
+  virtual void Empty() = 0;
+  virtual void Size() = 0;
+  virtual void Height() = 0;
+  virtual void Ancestor(int x) = 0;
+  virtual void Average(int x) = 0;
+  virtual void Rank(int x) = 0;
+  virtual void Erase(int x) = 0;
+  virtual void PrintTree() = 0;
+};
+
+#endif
