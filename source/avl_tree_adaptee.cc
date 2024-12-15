6ce18f1444d17d39d3cdf3d82cc4c87608780352
Lee Dong Ik <ehddlr27@gmail.com>
2024-12-15 23:59:41 +0900

    Feat(avl_tree_adaptee.cc): Search 구현

--- source/avl_tree_adaptee.cc
+++ source/avl_tree_adaptee.cc
@@ -2,6 +2,18 @@
 
 AVLTreeAdaptee::AVLTreeAdaptee() : root_(NULL) {}
 
+node* AVLTreeAdaptee::Search(node* cur_node, int key) {
+  if (cur_node == nullptr) return nullptr;
+
+  if (cur_node->key == key)
+    return cur_node;
+  else if (cur_node->key < key) {
+    return Search(cur_node->right, key);
+  } else {
+    return Search(cur_node->left, key);
+  }
+}
+
 int AVLTreeAdaptee::IsEmpty() {
   return root_ ? 0 : 1;  // 비어있으면 1, 아니면 0
 }
